bec60337c7b3ccaaec1840a509511dc9248c19a0
freedyest <freedyjob@gmail.com>
2025-10-15 06:42:34 +0700

    update sql

--- educourse.sql
+++ educourse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2025 at 10:29 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Waktu pembuatan: 15 Okt 2025 pada 01.40
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course`
+-- Struktur dari tabel `course`
 --
 
 CREATE TABLE `course` (
@@ -34,10 +34,10 @@ CREATE TABLE `course` (
   `nama_kelas` varchar(255) NOT NULL,
   `deskripsi` text NOT NULL,
   `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `course`
+-- Dumping data untuk tabel `course`
 --
 
 INSERT INTO `course` (`id`, `id_tutor`, `id_kategori`, `nama_kelas`, `deskripsi`, `harga`) VALUES
@@ -47,23 +47,23 @@ INSERT INTO `course` (`id`, `id_tutor`, `id_kategori`, `nama_kelas`, `deskripsi`
 (5, 2, 3, 'hi', 'Wawaaw', 10000),
 (6, 2, 3, 'dari postman nih', 'halo ini postman lohh', 10000),
 (7, 2, 3, 'dari postman nih2', 'halo ini postman lohh2', 100002),
-(8, 2, 3, 'dari postman nih2', 'halo ini postman lohh2', 100002),
-(9, 2, 3, 'dari postman nih2', 'halo ini postman lohh2', 300002),
+(8, 2, 2, 'dari postman nih2', 'halo ini postman lohh2', 100002),
+(9, 2, 2, 'dari postman nih2', 'halo ini postman lohh2', 300002),
 (14, 2, 2, 'dari  post testing nih ', 'testing postman', 200000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori_kelas`
+-- Struktur dari tabel `kategori_kelas`
 --
 
 CREATE TABLE `kategori_kelas` (
   `id_kategori` int(11) NOT NULL,
   `nama_kategori` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kategori_kelas`
+-- Dumping data untuk tabel `kategori_kelas`
 --
 
 INSERT INTO `kategori_kelas` (`id_kategori`, `nama_kategori`) VALUES
@@ -75,7 +75,7 @@ INSERT INTO `kategori_kelas` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tutor`
+-- Struktur dari tabel `tutor`
 --
 
 CREATE TABLE `tutor` (
@@ -83,10 +83,10 @@ CREATE TABLE `tutor` (
   `nama_tutor` varchar(255) NOT NULL,
   `pekerjaan_tutor` varchar(255) NOT NULL,
   `tempat_kerja` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tutor`
+-- Dumping data untuk tabel `tutor`
 --
 
 INSERT INTO `tutor` (`id_tutor`, `nama_tutor`, `pekerjaan_tutor`, `tempat_kerja`) VALUES
@@ -96,7 +96,7 @@ INSERT INTO `tutor` (`id_tutor`, `nama_tutor`, `pekerjaan_tutor`, `tempat_kerja`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -107,28 +107,21 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `verification_token` varchar(255) DEFAULT NULL,
   `is_verified` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id_user`, `fullname`, `username`, `password`, `email`, `verification_token`, `is_verified`) VALUES
-(1, 'Budi Santoso', 'budi123', '$2b$10$ZCVewRa3JV3kuqs9x/G.JeuUga6c85w7qliuyhoC0/lmcStl9ek7C', 'budi@example.com', NULL, 1),
-(2, 'fardy', 'fardy', '$2b$10$cC.lIl5lW/UKw2roI6GKhOWhrUX/BEkXlCmTTj6P6qQCThmP4OI1e', 'fardy@example.com', NULL, 1),
-(3, 'fardy', 'fredy', '$2b$10$mTK5GPysy/IxXjL.Tv/DNO3ZUvOhIzI9xkYzvE.PufjoAjwF7IDYe', 'fredy@gmail.com', NULL, 1),
-(4, 'Freedy', 'freedy123', '$2b$10$.BWgj8WrlDGXsJSe/nLDpe3aZbNk.L8WnC3uNQwpT648PkKVXNPMW', 'freedygenshin@gmail.com', NULL, 1),
-(5, 'Freedy', 'freedy123', '$2b$10$4gdFazRmAh1ZxBkEka.yS.SMS/ouX1Om22foVewtA1C3x7cNLl6a.', 'freedygenshin@gmail.com', NULL, 1),
-(6, 'Freedy', 'freedy123', '$2b$10$Ep7S2RiN/BfhaB1XrSSHXuQ1YHfh6xzicwTUEHRLzXwp.849LoTjy', 'freedyjob@gmail.com', NULL, 1),
-(7, 'Freedy', 'freedy', '$2b$10$ji4hXlEsIK/AKtkSXcAaPuskIOX/xh2zuIlZz.3AnV0QF5NkUMRh6', 'freedyjob@gmail.com', NULL, 1),
-(8, 'Freedy', 'freedy', '$2b$10$hIZEk7nxlyNHWxTOHaNadetkPDNYrqXCiMD8urRnaY/gQUmhFTizC', 'freedyjob@gmail.com', NULL, 1);
+(12, 'FreedyEstiawan', 'freedyest', '$2b$10$IvhcuTA1hNh/6ldvcmBUQOlVMQ1LsNRhZIe2YrGrbE4tnQNK3VPIu', 'freedyjob@gmail.com', NULL, 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `course`
+-- Indeks untuk tabel `course`
 --
 ALTER TABLE `course`
   ADD PRIMARY KEY (`id`),
@@ -136,57 +129,57 @@ ALTER TABLE `course`
   ADD KEY `id_kategori` (`id_kategori`);
 
 --
--- Indexes for table `kategori_kelas`
+-- Indeks untuk tabel `kategori_kelas`
 --
 ALTER TABLE `kategori_kelas`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `tutor`
+-- Indeks untuk tabel `tutor`
 --
 ALTER TABLE `tutor`
   ADD PRIMARY KEY (`id_tutor`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `course`
+-- AUTO_INCREMENT untuk tabel `course`
 --
 ALTER TABLE `course`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `kategori_kelas`
+-- AUTO_INCREMENT untuk tabel `kategori_kelas`
 --
 ALTER TABLE `kategori_kelas`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tutor`
+-- AUTO_INCREMENT untuk tabel `tutor`
 --
 ALTER TABLE `tutor`
   MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `course`
+-- Ketidakleluasaan untuk tabel `course`
 --
 ALTER TABLE `course`
   ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`),
